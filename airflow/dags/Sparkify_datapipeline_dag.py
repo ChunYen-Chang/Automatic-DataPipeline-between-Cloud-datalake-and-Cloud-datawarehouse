500aed0bdd2a44bbc9cd9057500b44b62d2535fd
ChunYen Chang <fikrusnk@gmail.com>
2020-03-23 22:14:28 +0800

    STYLE: Format the script

--- airflow/dags/Sparkify_datapipeline_dag.py
+++ airflow/dags/Sparkify_datapipeline_dag.py
@@ -8,137 +8,137 @@ from helpers import SqlQueries
 # define the default argument for DAG
 default_args = {
     'owner': 'ChunYen-Chang',
-    'start_date': datetime(2019, 1, 12),
-    'depends_on_past': False,
+    'start_date': datetime(2019, 1, 1),
+    'depends_on_past': True,
     'retries': 3,
     'retry_delay': timedelta(minutes=5),
-    'email_on_retry': False,
+    'email_on_failure': True,
 }
 
 # instantiate an DAG object
-dag = DAG('Sparkify_data_pipeline_dag',
-          default_args=default_args,
-          description='Load event and song data from S3, transform data, and save transformed data in Redshift',
-          schedule_interval='0 * * * *',
-          catchup=False,
-          max_active_runs=1
+dag = DAG('datapipeline_s3_and_redshift_dag',
+          default_args = default_args,
+          description = 'Load event and song data from S3, transform data, and save transformed data in Redshift',
+          schedule_interval = '0 * * * *',
+          catchup = False,
+          max_active_runs = 3
         )
 
 # define start_operator task
 start_operator = DummyOperator(
-    task_id='Begin_execution',  
-    dag=dag
+    task_id = 'Begin_execution',  
+    dag = dag
 )
 
 # define stage_events_to_redshift task
 stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
-    dag=dag,
-    redshift_conn_id='redshift',
-    aws_credentials_id='aws_credentials',
-    table='staging_events',
-    create_table_sql=SqlQueries.staging_events_table_create,
-    s3_bucket='udacity-dend',
-    s3_key='log_data',
+    task_id = 'Stage_events',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    aws_credentials_id = 'aws_credentials',
+    table = 'staging_events',
+    create_table_sql = SqlQueries.staging_events_table_create,
+    s3_bucket = 'musiccompany_clean_data',
+    s3_key = 'log_data',
 )
 
 # define stage_songs_to_redshift task
 stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
-    dag=dag,
-    redshift_conn_id='redshift',
-    aws_credentials_id='aws_credentials',
-    table='staging_songs',
-    create_table_sql=SqlQueries.staging_songs_table_create,
-    s3_bucket='udacity-dend',
-    s3_key='song_data',
+    task_id = 'Stage_songs',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    aws_credentials_id = 'aws_credentials',
+    table = 'staging_songs',
+    create_table_sql = SqlQueries.staging_songs_table_create,
+    s3_bucket = 'musiccompany_clean_data',
+    s3_key = 'song_data',
 )
 
 # define load_songplays_table task
 load_songplays_table = LoadFactOperator(
-    task_id='Load_songplays_fact_table',
-    dag=dag,
-    redshift_conn_id='redshift',
-    table='songplays',
-    create_table_sql=SqlQueries.songplays_table_create,
-    insert_table_sql=SqlQueries.songplay_table_insert,
-    mode='append' 
+    task_id = 'Load_songplays_fact_table',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'songplays',
+    create_table_sql = SqlQueries.songplays_table_create,
+    insert_table_sql = SqlQueries.songplay_table_insert,
+    mode = 'append' 
 )
 
 # define load_user_dimension_table task
 load_user_dimension_table = LoadDimensionOperator(
-    task_id='Load_user_dim_table',
-    dag=dag,
-    redshift_conn_id='redshift',
-    table='users',
-    create_table_sql=SqlQueries.users_table_create,
-    insert_table_sql=SqlQueries.user_table_insert,
-    mode='overwrite'       
+    task_id = 'Load_user_dim_table',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'users',
+    create_table_sql = SqlQueries.users_table_create,
+    insert_table_sql = SqlQueries.user_table_insert,
+    mode = 'overwrite'       
 )
 
 # define load_song_dimension_table task
 load_song_dimension_table = LoadDimensionOperator(
-    task_id='Load_song_dim_table',
-    dag=dag,
-    redshift_conn_id='redshift',
-    table='songs',
-    create_table_sql=SqlQueries.songs_table_create,
-    insert_table_sql=SqlQueries.song_table_insert,
-    mode='overwrite'  
+    task_id = 'Load_song_dim_table',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'songs',
+    create_table_sql = SqlQueries.songs_table_create,
+    insert_table_sql = SqlQueries.song_table_insert,
+    mode = 'overwrite'  
 )
 
 # define load_artist_dimension_table task
 load_artist_dimension_table = LoadDimensionOperator(
-    task_id='Load_artist_dim_table',
-    dag=dag,
-    redshift_conn_id='redshift',
-    table='artists',
-    create_table_sql=SqlQueries.artists_table_create,
-    insert_table_sql=SqlQueries.artist_table_insert,
-    mode='overwrite'  
+    task_id = 'Load_artist_dim_table',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'artists',
+    create_table_sql = SqlQueries.artists_table_create,
+    insert_table_sql = SqlQueries.artist_table_insert,
+    mode = 'overwrite'  
 )
 
 # define load_time_dimension_table task
 load_time_dimension_table = LoadDimensionOperator(
-    task_id='Load_time_dim_table',
-    dag=dag,
-    redshift_conn_id='redshift',
-    table='time',
-    create_table_sql=SqlQueries.time_table_create,
-    insert_table_sql=SqlQueries.time_table_insert,
-    mode='overwrite'  
+    task_id = 'Load_time_dim_table',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'time',
+    create_table_sql = SqlQueries.time_table_create,
+    insert_table_sql = SqlQueries.time_table_insert,
+    mode = 'overwrite'  
 )
 
 # define run_quality_checks_songs task
 run_quality_checks_songs = DataQualityOperator(
-    task_id='Data_quality_checks_songs',
-    dag=dag,
-    redshift_conn_id='redshift',
-    table='songs'
+    task_id = 'Data_quality_checks_songs',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'songs'
 )
 
 # define run_quality_checks_users task
 run_quality_checks_users = DataQualityOperator(
-    task_id='Data_quality_checks_users',
-    dag=dag,
-    redshift_conn_id='redshift',
-    table='users'
+    task_id = 'Data_quality_checks_users',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'users'
 )
 
 # define run_quality_checks_artists task
 run_quality_checks_artists = DataQualityOperator(
-    task_id='Data_quality_checks_artists',
-    dag=dag,
-    redshift_conn_id='redshift',
-    table='artists'
+    task_id = 'Data_quality_checks_artists',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'artists'
 )
 
 # define run_quality_checks_time task
 run_quality_checks_time = DataQualityOperator(
-    task_id='Data_quality_checks_time',
-    dag=dag,
-    redshift_conn_id='redshift',
-    table='time'
+    task_id = 'Data_quality_checks_time',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'time'
 )
 
 # define end_operator task
